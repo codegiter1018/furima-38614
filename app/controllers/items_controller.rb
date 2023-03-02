55028030881df28279639a17c0a67e51f4f1c60b
codegiter1018 <tekku1te2tete@gmail.com>
2023-03-02 17:57:48 +0900

    商品情報編集機能の実装
    ルボコップ済

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
@@ -19,6 +19,22 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
   end
 
   private
