99d676ea6a4600f250083c027af85538c06f9e27
codegiter1018 <tekku1te2tete@gmail.com>
2023-03-05 20:01:54 +0900

    削除機能の実装
    ルボコップ済

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
@@ -22,9 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id != current_user.id
-      redirect_to root_path
-    end
+    return unless @item.user_id != current_user.id
+
+    redirect_to root_path
   end
 
   def update
@@ -33,6 +33,15 @@ class ItemsController < ApplicationController
       redirect_to item_path(item_params)
     else
       render 'edit'
+    end
+  end
+
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
 
