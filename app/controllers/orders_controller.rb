940163ba680c3ba9fbd568b844e19231dbfc14fc
codegiter1018 <tekku1te2tete@gmail.com>
2023-03-08 17:40:22 +0900

    ビューの仮設定

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,48 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  
+  
+  def index
+   if current_user.id == @item.user_id || @item.order.present?
+      @item_order = ItemOrder.new
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  def create
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      pay_item
+      @item_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
+  def order_params
+    params.require(:item_order).permit(:postcode, :prefecture_id, :municipalities, :address, :buildingname, :telephonenumber
+    ).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,        
+      card: order_params[:token], 
+      currency: 'jpy'             
+    )
+  end
+
+
+
+end
